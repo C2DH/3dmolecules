6753bf065115ea2191441aea02b320cbdfcc3bfc
donsiamese <donsiamese@gmail.com>
2025-03-09 13:02:54 +0100

    Update NicotineModel.jsx

--- src/modelComps/NicotineModel.jsx
+++ src/modelComps/NicotineModel.jsx
@@ -108,21 +108,19 @@ const NicotineModel = forwardRef(({ position, ...props }, ref) => {
       </group>
       <Sparkles count={60} size={1} scale={5} speed={0.1} opacity={0.3} />
       <EffectComposer>
+        <Bloom
+          intensity={1.5} // Adjust the intensity of the glow
+          kernelSize={3} // Adjust the size of the glow
+          luminanceThreshold={0.01} // Adjust the threshold for what gets glowing
+          luminanceSmoothing={0.01} // Adjust the smoothness of the glow
+        />
         {isBigScreen ? (
-          <>
-            <Bloom
-              intensity={1.5} // Adjust the intensity of the glow
-              kernelSize={3} // Adjust the size of the glow
-              luminanceThreshold={0.01} // Adjust the threshold for what gets glowing
-              luminanceSmoothing={0.01} // Adjust the smoothness of the glow
-            />
-            <DepthOfField
-              focusDistance={0} // Distance to the focus target
-              focalLength={0.6} // Strength of the blur
-              bokehScale={4} // Scale of the bokeh effect
-              height={480} // Optional: Height of the effect (can be adjusted)
-            />
-          </>
+          <DepthOfField
+            focusDistance={0} // Distance to the focus target
+            focalLength={0.6} // Strength of the blur
+            bokehScale={4} // Scale of the bokeh effect
+            height={480} // Optional: Height of the effect (can be adjusted)
+          />
         ) : null}
       </EffectComposer>
     </>
