497e65edee20e2d4c50d12a6251465ecaf42ed00
donsiamese <donsiamese@gmail.com>
2025-03-12 11:17:04 +0100

    remove bloom effect

--- src/Ui/SpecialEffects.jsx
+++ src/Ui/SpecialEffects.jsx
@@ -9,19 +9,22 @@ const SpecialEffects = () => {
     <>
       {isBigScreen ? <Sparkles count={60} size={1} scale={5} speed={0.1} opacity={0.3} /> : null}
       <EffectComposer>
-        <Bloom
-          intensity={1.5} // Adjust the intensity of the glow
-          kernelSize={2} // Adjust the size of the glow
-          luminanceThreshold={0.01} // Adjust the threshold for what gets glowing
-          luminanceSmoothing={0.01} // Adjust the smoothness of the glow
-        />
         {isBigScreen ? (
-          <DepthOfField
-            focusDistance={0} // Distance to the focus target
-            focalLength={1} // Strength of the blur
-            bokehScale={4} // Scale of the bokeh effect
-            height={480} // Optional: Height of the effect (can be adjusted)
-          />
+          <>
+            <Bloom
+              intensity={1.5} // Adjust the intensity of the glow
+              kernelSize={2} // Adjust the size of the glow
+              luminanceThreshold={0.01} // Adjust the threshold for what gets glowing
+              luminanceSmoothing={0.01} // Adjust the smoothness of the glow
+            />
+
+            <DepthOfField
+              focusDistance={0} // Distance to the focus target
+              focalLength={1} // Strength of the blur
+              bokehScale={4} // Scale of the bokeh effect
+              height={480} // Optional: Height of the effect (can be adjusted)
+            />
+          </>
         ) : null}
       </EffectComposer>
     </>
