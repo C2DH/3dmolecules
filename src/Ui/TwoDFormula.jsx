f3cdee37046cd2bda58a2ebcf92b8cb8a6776d4f
donsiamese <donsiamese@gmail.com>
2025-03-14 12:25:40 +0100

    mobile size update

--- src/Ui/TwoDFormula.jsx
+++ src/Ui/TwoDFormula.jsx
@@ -3,16 +3,18 @@ import CaffeineSvg from '../Svg/Chemical/CaffeineSvg'
 import NicotineSvg from '../Svg/Chemical/NicotineSvg'
 import BisphenolSSvg from '../Svg/Chemical/BisphenolSSvg'
 import DDTSvg from '../Svg/Chemical/DdtSvg'
+import { useMediaQuery } from 'react-responsive'
 
 const TwoDFormula = ({ pathname }) => {
+  const isBigScreen = useMediaQuery({ query: '(min-width: 640px)' })
   return (
     <>
       <div className="ChemicalSvg flex justify-center opacity-50 w-full fixed top-10 z-1">
-        {pathname === '/trifluoroacetic_acid' ? <TrifluoroaceticAcidSvg width={200} /> : null}
-        {pathname === '/caffeine' ? <CaffeineSvg width={220} /> : null}
-        {pathname === '/nicotine' ? <NicotineSvg width={240} /> : null}
-        {pathname === '/bisphenol_s' ? <BisphenolSSvg width={320} /> : null}
-        {pathname === '/ddt' ? <DDTSvg width={260} /> : null}
+        {pathname === '/trifluoroacetic_acid' ? <TrifluoroaceticAcidSvg width={isBigScreen ? 200 : 130} /> : null}
+        {pathname === '/caffeine' ? <CaffeineSvg width={isBigScreen ? 220 : 140} /> : null}
+        {pathname === '/nicotine' ? <NicotineSvg width={isBigScreen ? 240 : 150} /> : null}
+        {pathname === '/bisphenol_s' ? <BisphenolSSvg width={isBigScreen ? 320 : 190} /> : null}
+        {pathname === '/ddt' ? <DDTSvg width={isBigScreen ? 260 : 160} /> : null}
       </div>
     </>
   )
