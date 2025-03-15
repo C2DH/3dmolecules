8134ec3d531ba6fdc8a3bbc11f4c6a75d1779a53
donsiamese <donsiamese@gmail.com>
2025-03-15 20:49:52 +0100

    remove autoscroll comp from index

--- src/App.jsx
+++ src/App.jsx
@@ -76,15 +76,17 @@ function App() {
     <>
       {}
       {isBigScreen && pathname !== '/' ? (
-        <button
-          className="autoplay-button auto-play-trigger fixed flex justify-center items-center z-10"
-          onClick={autoPlayTrigger}
-        >
-          <Autoplay className={'absolute'} width={100} />
-          <span className="text-center absolute'">{isPaused ? 'OFF' : 'ON'}</span>
-        </button>
+        <>
+          <button
+            className="autoplay-button auto-play-trigger fixed flex justify-center items-center z-10"
+            onClick={autoPlayTrigger}
+          >
+            <Autoplay className={'absolute'} width={100} />
+            <span className="text-center absolute'">{isPaused ? 'OFF' : 'ON'}</span>
+          </button>
+          <AutoScroller />
+        </>
       ) : null}
-      <AutoScroller />
       <ModalWindow closeModal={closeModal} isModalImage={isModalImage} isModalVisible={isModalVisible} />
       <Header scrollToTop={scrollToTop} />
 
