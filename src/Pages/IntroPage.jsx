28a1d6eb2c0f9a8f42339e3bae7415057c98e039
donsiamese <donsiamese@gmail.com>
2025-03-20 21:56:31 +0100

    intro page translation

--- src/Pages/IntroPage.jsx
+++ src/Pages/IntroPage.jsx
@@ -8,8 +8,19 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import QrCode from '../Ui/QrCode'
 import { useTranslation } from 'react-i18next'
 
+const getTranslatable = (localizedObject, currentLanguage, fallbackLanguage = 'en') => {
+  if (typeof localizedObject === 'string') {
+    return localizedObject
+  }
+  if (localizedObject && typeof localizedObject === 'object') {
+    return localizedObject[currentLanguage] || localizedObject[fallbackLanguage] || ''
+  }
+  return ''
+}
+
 const IntroPage = () => {
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
+
   return (
     <div className="IntroPage">
       <QrCode />
@@ -20,8 +31,8 @@ const IntroPage = () => {
       <main className="flex w-full overflow-hidden flex-col justify-center items-center p-4">
         <HelmetProvider>
           <Helmet>
-            <title>3D Stories</title>
-            <meta name="description" content="Telling stories about chemicals." data-rh="true" />
+            <title>{t('3D Stories')}</title>
+            <meta name="description" content={t('Telling stories about chemicals.')} data-rh="true" />
             <meta name="keywords" content="LCSB, C²DH" />
           </Helmet>
         </HelmetProvider>
@@ -29,7 +40,7 @@ const IntroPage = () => {
           {IntroContent?.sections?.map((d, i) =>
             i === 0 ? (
               <div key={d.path ?? i} className="intro relative">
-                <h1 dangerouslySetInnerHTML={{ __html: d.title }} />{' '}
+                <h1 dangerouslySetInnerHTML={{ __html: getTranslatable(d.title, i18n.language, 'en') }} />
                 <Link to="/trifluoroacetic_acid">
                   <Button
                     className="mt-5 w-full md:w-auto sm:mr-0 md:mr-3 xl2:mr-3 pointer-events-auto"
@@ -47,16 +58,23 @@ const IntroPage = () => {
               key={d.path ?? i}
               className="flex intro-description relative flex-col w-screen justify-center items-center"
             >
-              {console.debug('PATH', arr)}
               <div
                 className={`max-w-[800px] p-6 md:p-10 ${i === 0 ? 'mt-[10vh]' : ''} justify-start ${i === 5 ? 'hidden' : 'block'} ${i === 4 ? 'mb-[10vh]' : ''}`}
               >
-                <h2 className="mb-[1rem] self-start">{d.title === 'Team' ? null : d.title}</h2>
-                <p className="intro-content flex-grow" dangerouslySetInnerHTML={{ __html: d.description }}></p>
+                <h2 className="mb-[1rem] self-start">
+                  {getTranslatable(d.title, i18n.language, 'en') === 'Team' ||
+                  getTranslatable(d.title, i18n.language, 'en') === 'Équipe'
+                    ? null
+                    : getTranslatable(d.title, i18n.language, 'en')}
+                </h2>
+                <p
+                  className="intro-content flex-grow"
+                  dangerouslySetInnerHTML={{ __html: getTranslatable(d.description, i18n.language, 'en') }}
+                ></p>
               </div>
             </section>
           ))}
-        <h2 className="mb-[1rem] mt-[10rem] relative">Team</h2>
+        <h2 className="mb-[1rem] mt-[10rem] relative">{t('team')}</h2>
         {IntroContent?.sections
           .filter(section => section.team)
           .map((section, i) => (
@@ -65,24 +83,16 @@ const IntroPage = () => {
                 <div key={j}>
                   <Team
                     key={member.name}
-                    name={member.name}
+                    name={getTranslatable(member.name, i18n.language, 'en')}
                     img={member.img}
-                    role={member.role}
-                    uni={member.uni}
+                    role={getTranslatable(member.role, i18n.language, 'en')}
+                    uni={getTranslatable(member.uni, i18n.language, 'en')}
                     link={member.link}
                   />
                 </div>
               ))}
             </div>
           ))}
-        {/* <div className="introduction-links relative flex flex-wrap justify-center">
-          <a className="mx-3" href="https://github.com/C2DH/3dmolecules" target="_blank">
-            GitHub-Repo
-          </a>
-          <a className="mx-3" href="https://github.com/C2DH/3dmolecules" target="_blank">
-           3D molecules
-          </a>
-        </div> */}
       </main>
     </div>
   )
